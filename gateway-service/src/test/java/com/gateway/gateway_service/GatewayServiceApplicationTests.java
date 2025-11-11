1102879802da4f9d2f418e01bb44818865c395b9
Hugo Benitez <hugo.boca16@gmail.com>
2025-11-11 16:28:25 -0300

    configuracion gateway

---
+++ gateway-service/src/test/java/com/gateway/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.gateway.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootTest
+@EnableDiscoveryClient
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
